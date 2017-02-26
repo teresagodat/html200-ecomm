67bfcffff61d30d059d671ba88727e849d1ef64e
Teresa Godat <teresagodat@yahoo.com>
2017-02-25 21:29:47 -0800

    fixed formatting

--- js/script.js
+++ js/script.js
@@ -49,27 +49,33 @@ var products = [
   }
  ]
 
+ 
+for(var key in products) { 
+console.log(key); 
+console.log(products[key]);
+}
+
 function selValue(){
     console.log(document.getElementById('selection').value);
     event.preventDefault();
 }
 
-//function for sum cart total 
-
-var arr = ["one", "two"]
-
-//TODO: trigger on change of cart contents
-function sumPrices(cartArray) {
-  // for loop through array, sum value of price attribute for each object
-    var total = 0;
-  
-    for(var i=0; i<cartArray.length; i++) {
-    
-      if(cartArray[i].price) {
-        total = total + cartArray[i].price;
-      }
-  }
-  
-  //TODO: print total as HTML to page, next to cart icon
-  console.log(total);
-}
+////function for sum cart total 
+//
+//var arr = ["one", "two"]
+//
+////TODO: trigger on change of cart contents
+//function sumPrices(cartArray) {
+//  // for loop through array, sum value of price attribute for each object
+//    var total = 0;
+//  
+//    for(var i=0; i<cartArray.length; i++) {
+//    
+//      if(cartArray[i].price) {
+//        total = total + cartArray[i].price;
+//      }
+//  }
+//  
+//  //TODO: print total as HTML to page, next to cart icon
+//  console.log(total);
+//}
